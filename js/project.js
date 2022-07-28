785fc546cde5e402da0c6771f48badaab5f3f9e9
Matthias <r0842956@student.thomasmore.be>
2022-07-28 13:33:37 +0200

    final version nog nakijken

--- js/project.js
+++ js/project.js
@@ -2,117 +2,110 @@ let submitButton = document.getElementById("submit")
 let ErrorMessages = []
 let message
 
-
-validateForm = () => {
+function validateForm(){
     checkEmptyField("voornaam", "Het veld voornaam is vereist.<br>")
 
     checkEmptyField("naam", "Het veld naam is vereist.<br>")
 
     checkEmptyField("gebruikersnaam", "Het veld gebruikersnaam is vereist.<br>")
 
-    valideerEmail("mailadres")
-
-    if (document.getElementById("mailadres").value == "") {
-        checkEmptyField("mailadres", "Het veld E-mailadres is vereist.<br>")
-    } else if (valideerEmail("mailadres") == false) {
-        ErrorMessages.push("E-mailadres is niet correct.<br>")
-    }
-
-    valideerEmail("mailadres")
-
-    controleerWachtwoord("wachtwoord", "HerhaalWachtwoord")
+    checkEmptyField("mailadres", "Het veld email is vereist.<br>")
 
     checkEmptyField("adres", "Het veld adres is vereist.<br>")
 
+    controleerWachtwoord("wachtwoord", "HerhaalWachtwoord")
+
     checkLand("Land", 'Het veld land is vereist.<br>')
+
     checkProvincie("Provincie", 'Het veld provincie is vereist.<br>')
 
-    checkPostcode("postcode")
+    checkPostcode("postcode", 'Het veld postcode is vereist.<br>')
 
     checkAlgemeneVoorwaarden("AlgemeneVoorwaarden")
 
     if (ErrorMessages.length == 0) {
         messages = '<div class="alert alert-success" role="alert">'
         messages += '<h4 class="alert-heading">Goed gedaan</h4>'
-        messages += '<p>Alles goed ingevuld, je bent nu geregistreerd!<p>'
-        messages += '</div>'
+        messages += '<p>Alles is goed ingevuld, u bent nu geregistreerd!<p>'
+        messages += '<p>Bedankt voor uw tijd om dit formulier in te vullen.<p></div>'
+       
         messages += '<div class="alert alert-info" role="alert">'
-        messages += '<h4 class="alert-heading">Betaalmethode</h4>'
+        messages += '<h4 class="alert-heading">Uw betaalmethode</h4>'
         messages += valideerBetaling("betaaloptie")
         messages += '</div>'
-    } else {
+        
+        } else {
+    
         messages = '<div class="alert alert-danger" role="alert">'
-        messages += '<h4 class="alert-heading">Dit zijn jouw Errors, die je nog in orde moet brengen.</h4>'
+        messages += '<h4 class="alert-heading">Dit zijn jouw errors, die je nog in orde moet brengen.</h4>'
         messages += ErrorMessages.join('')
         messages += '</div>'
-        // https://www.youtube.com/watch?v=In0nB0ABaUk&t=315s
+        
 
     }
     document.getElementById('foutmeldingen').innerHTML = messages
     ErrorMessages = []
+    // https://www.youtube.com/watch?v=In0nB0ABaUk&t
 }
 
+    function checkEmptyField (VELD, MESSAGE){
+        let tekst = document.getElementById(VELD).value
+        if (tekst == "") {
+        ErrorMessages.push(MESSAGE)
+    }
+}
 
-checkLand = (veldLand) => {
-    let land = document.getElementById(veldLand).value
-    
-    if (land == "Kies een Land") {
+    function checkLand(LAND){
+        let land = document.getElementById(LAND).value
+        if (land == "Kies een Land") {
         ErrorMessages.push("Het veld land is vereist.<br>")
     }
 }
-checkProvincie = (veldProvincie) => {
-   let provincie = document.getElementById(veldProvincie).value
-   
-   if (provincie == "Kies een Provincie") {
-       ErrorMessages.push("Het veld provincie is vereist.<br>")
-   }
-}
 
-checkEmptyField = (veld, melding) => {
-    let invoer = document.getElementById(veld).value
-    if (invoer == "") {
-        ErrorMessages.push(melding)
+    function checkProvincie(PROVINCIE) {
+        let provincie = document.getElementById(PROVINCIE).value
+        if (provincie == "Kies een Provincie") {
+        ErrorMessages.push("Het veld provincie is vereist.<br>")
     }
 }
 
 
-valideerBetaling = (veld) => {
-    const radioButtons = document.getElementsByName(veld);
-    let selectedbetaalmethode;
+
+
+    function valideerBetaling(VELD){
+        const radioButtons = document.getElementsByName(VELD);
+        let selectedbetaalmethode;
     //https://www.javascripttutorial.net/javascript-dom/javascript-radio-button/
-    for (const radioButton of radioButtons) {
-        if (radioButton.checked) {
-            selectedbetaalmethode = radioButton.value;
-            break;
-        }
+        for (const radioButton of radioButtons) {
+        if (radioButton.checked) {selectedbetaalmethode = radioButton.value;break;}
     }
     return "Je betalingswijze is " + selectedbetaalmethode
    
 }
 
 
-checkPostcode = (veld) => {
-    let invoer = document.getElementById(veld).value
-    if (invoer == "") {
+    function checkPostcode(VELD) {
+        let tekst = document.getElementById(VELD).value
+        if (tekst == "") {
         ErrorMessages.push("Het veld postcode is vereist.<br>")
-    } else if (invoer > 9999 || invoer < 1000) {
+        } else if (tekst > 9999 || tekst < 1000) {
         ErrorMessages.push("De waarde van uw postcode moet tussen 1000 en 9999 liggen.<br>")
     }
 
 }
 
 
-controleerWachtwoord = (veldWachtwoord, veldHerhaal) => {
-    let wachtwoord = document.getElementById(veldWachtwoord).value
-    let herhaal = document.getElementById(veldHerhaal).value
-    if (wachtwoord == "" || herhaal == "") {
-        checkEmptyField(veldWachtwoord, "Het veld wachtwoord is vereist.<br>")
-        checkEmptyField(veldHerhaal, "Het herhalende wachtwoord is vereist.<br>")
-    } else {
-        if (wachtwoord != herhaal) {
+    function controleerWachtwoord(VELDWW, VELDHerhaalWW) {
+        let wachtwoord = document.getElementById(VELDWW).value
+        let herhaal = document.getElementById(VELDHerhaalWW).value
+        if (wachtwoord == "" || herhaal == "") {
+            checkEmptyField(VELDWW, "Het veld wachtwoord is vereist.<br>")
+            checkEmptyField(VELDHerhaalWW, "Het herhalende wachtwoord is vereist.<br>")
+        } else {
+            if (wachtwoord != herhaal) {
             ErrorMessages.push("Je wachtwoorden zijn niet hetzelfde.<br>")
         }
-        if (wachtwoord.length < 8) {
+            if (wachtwoord.length < 8) {
             ErrorMessages.push("Je wachtwoord moet minstens 8 karakters lang zijn.<br>")
         }
     }
@@ -120,24 +113,12 @@ controleerWachtwoord = (veldWachtwoord, veldHerhaal) => {
 }
 
 
-checkAlgemeneVoorwaarden = (veld) => {
-    let invoer = document.getElementById(veld)
-    if (invoer.checked == false) {
+    function checkAlgemeneVoorwaarden(VELD) {
+        let tekst = document.getElementById(VELD)
+        if (tekst.checked == false) {
         ErrorMessages.push("Je moet de algemene voorwaarden accepteren.<br>")
     }
 }
 
-
-valideerEmail = (emailadres) => {
-    let email = document.getElementById(emailadres)
-    var validRegex = /^[a-zA-Z0-9!#$%&'*+/=?^_`{|}~]+[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9]+(?:\.[a-zA-Z0-9-]+)*$/;
-    //https://www.simplilearn.com/tutorials/javascript-tutorial/email-validation-in-javascript#how_to_validate_email_using_javascript
-    if (email.value.match(validRegex)) {
-        return true;
-    } else {
-        return false;
-    }
-
-}
 
 submitButton.addEventListener("click", validateForm)
